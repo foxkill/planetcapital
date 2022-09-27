3c1bc4f2d287eb347881936c4e5045792e4652ff
martinpfh <martin@pfh.de>
2022-09-27 12:28:36 +0200

    Add TickersController

---
+++ app/Http/Controllers/TickersController.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Ticker;
+use Illuminate\Http\Request;
+
+class TickersController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index(Request $request)
+    {
+        return view('layouts.app', ['tickers' => []]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Ticker  $ticker
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Request $request, Ticker $ticker)
+    {
+        $this->validate($request, ['ticker' => 'required']);
+
+        if ($request->has('ticker')) {
+            dd($request);
+        } else {
+            redirect()->back();
+        }
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Ticker  $ticker
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Ticker $ticker)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Ticker  $ticker
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Ticker $ticker)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Ticker  $ticker
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Ticker $ticker)
+    {
+        //
+    }
+}
