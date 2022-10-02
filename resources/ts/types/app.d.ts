927f4d0278aa8dfaa25e9d3544278295582dbbd2
martinpfh <martin@pfh.de>
2022-10-02 11:48:41 +0000

    Add own types

--- resources/ts/types/app.d.ts
+++ resources/ts/types/app.d.ts
@@ -1,8 +1,21 @@
-export {};
+export {Ticker, Information};
 
 declare global {
     interface Window {
         _: any;
         axios: any;
     }
+}
+
+declare interface Ticker {
+    id: number;
+    tikr: string;
+    name: string;
+    exchange_id: number;
+}
+
+declare interface Information {
+    data: Ticker[];
+    loading: boolean;
+    err: any;
 }
