3533ff64a6a0f6e2c2c7291214512a5317e8e006
martinpfh <martin@pfh.de>
2022-10-04 09:17:26 +0000

    Remove not needed imports

--- resources/ts/Shared/Layout.tsx
+++ resources/ts/Shared/Layout.tsx
@@ -2,8 +2,6 @@ import React from 'react';
 import Hero from './Hero';
 import Footer from './Footer';
 import Navbar from './Navbar';
-import Info from './Info';
-import useFetch from '../useFetch';
 
 const Layout = () => {
     return (
