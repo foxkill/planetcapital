b3886570b2062abf5ca09ecae9f8ddb993b019fd
martinpfh <martin@pfh.de>
2022-10-04 09:20:51 +0000

    Remove Component from Hero

--- resources/ts/Shared/Hero/Hero.tsx
+++ resources/ts/Shared/Hero/Hero.tsx
@@ -1,25 +1,19 @@
-import React from "react";
+import { TickerSearch } from '../TickerSearch/TickerSearch';
+import { Cards } from '../Cards/Cards';
+import React, { useEffect, useRef, useState } from "react";
 import Tickerlist from "../Tickerlist";
+import useFetch from '../../useFetch';
 
-export function Hero({ }) {
-    return <div className="hero min-h-screen bg-base-200">
-        <div className="hero-content flex-col lg:flex-row-reverse">
-            <img src="images/banner.webp" className="max-w-sm rounded-lg shadow-2xl" />
-            <div>
-                <h1 className="text-5xl font-bold">Search for your favorite ticker here...</h1>
-                <p className="py-6"></p>
-                <div className="form-control">
-                    <div className="input-group">
-                        <input type="text" placeholder="Search, i. e. AAPL..." className="input input-bordered" list="tickerlist" required/>
-                        <button className="btn btn-square">
-                            <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" /></svg>
-                        </button>
-                    </div>
-                    <Tickerlist />
+export function Hero(props: { showCards: boolean }) {
+    return (
+        <>
+            <div className="hero min-h-[75vh] bg-base-200">
+                <div className="hero-content flex-col lg:flex-row-reverse">
+                    <TickerSearch />
                 </div>
             </div>
-        </div>
-    </div>;
+        </>
+    )
 }
 
 export default Hero
