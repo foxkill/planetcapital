db0a6c8e9f1a46e1bdcd0608a1d7883a5c05c2b2
martinpfh <martin@pfh.de>
2022-10-08 13:31:02 +0000

    Define new media breakpoints

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,35 @@
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
   content: [
     "./resources/**/*.blade.php",
     "./resources/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
+    // screens: {
+    //   sm: "480px",
+    //   md: "768px",
+    //   lg: "976px",
+    //   xl: "1440px"
+    // },
     screens: {
-      sm: "480px",
+      xs: '480px',
+      sm: "640px",
       md: "768px",
-      lg: "976px",
-      xl: "1440px"
+      lg: "1024px",
+      xl: "1280px",
+      "2xl": "1536px" 
     },
+
     extend: {
+      gridTemplateColumns: {
+        'child-size-lg': 'repeat(4, minmax(15rem, 15rem))',
+        'child-size-md': 'repeat(3, minmax(15rem, 15rem))',
+        'child-size-sm': 'repeat(2, minmax(15rem, 15rem))',
+        'child-size-xs': 'repeat(1, minmax(15rem, 15rem))',
+      },
       fontFamily: {
-          montserrat: [ "Montserrat", "sans-serif" ],
+        montserrat: ["Montserrat", "sans-serif"],
       },
       colors: {
         "search-field": "#f1f2f3",
