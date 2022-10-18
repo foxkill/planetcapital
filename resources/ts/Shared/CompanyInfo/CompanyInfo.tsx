130e423fe077deeb933caedb23aa6f8f5c2b4ed3
martinpfh <martin@pfh.de>
2022-10-18 16:05:14 +0000

    Embed fetch into component

--- resources/ts/Shared/CompanyInfo/CompanyInfo.tsx
+++ resources/ts/Shared/CompanyInfo/CompanyInfo.tsx
@@ -4,17 +4,39 @@
 // https://github.com/foxkill/planetcapital
 // Closed Source
 //
-import React, { ReactNode } from 'react'
+import fetchProfile from '@/planetapi/fetch.profile'
+import { IProfile } from '@/types/profile'
+import React, { useState } from 'react'
+import { useQuery } from 'react-query'
 import { useSecurity } from '../SecurityContext/SecurityContext'
+import Spinner from '../Spinner'
 
-interface ICompanyInfoProps {
-    children: ReactNode
-}
+function CompanyInfo(): JSX.Element {
+    const { symbol, exchange } = useSecurity().context
 
-function CompanyInfo(props: ICompanyInfoProps): JSX.Element {
-    const ctx = useSecurity()
-    const { image, symbol, exchange, price, changes } = ctx.context
-    // const imageUrl = (ctx.context.information.loading) ? <Spinner></Spinner> : image
+    const [companyName, setCompanyName] = useState("")
+    const [price, setPrice] = useState(0.0) 
+    const [changes, setChanges] = useState(0.0)
+    const [image, setImage] = useState("")
+
+    const { isLoading, isError } = useQuery<IProfile>(
+        [
+            ["profile", symbol, exchange].join("-"),
+            { symbol: symbol, exchange: exchange}
+        ],
+        fetchProfile,
+        {
+            enabled: Boolean(symbol && exchange),
+            retry: false,
+            onSuccess: (data: IProfile): void => { 
+                setCompanyName(data.companyName)
+                setPrice(data.price)
+                setChanges(data.changes)
+                setImage(data.image)
+            }
+        }
+    )
+
     const color = changes < 0 ? { color: 'red' } : { color: 'green' }
 
     function toPercentage(price: number, change: number): number {
@@ -27,19 +49,16 @@ function CompanyInfo(props: ICompanyInfoProps): JSX.Element {
     const sign = changes > 0 ? '+' : ''
 
     return (
-        <div className="stats shadow">
+        <div className={`stats shadow ${symbol ? '' : ' hidden'}`}>
             <div className="stat">
                 <div className="stat-figure text-secondary">
                     <div className="avatar">
                         <div className="w-24 rounded-full ring-1 ring-slate-400 ring-offset-base-100 ring-offset-4">
-                            {
-                                !ctx.context.information.loading &&
-                                <img src={image} />
-                            }
+                            { isLoading ? <Spinner height={24} width={24}></Spinner> : <img src={image} /> }
                         </div>
                     </div>
                 </div>
-                <div className="stat-value">{props.children}</div>
+                <div className="stat-value">{companyName}</div>
                 <div className="stat-title">{exchange.toUpperCase() + ":" + symbol.toUpperCase()}</div>
                 <div className="stat-desc">{price + " USD "}<span style={color}>{sign + changes.toPrecision(3) + " USD "}({sign + percentageChange}%)</span></div>
             </div>
