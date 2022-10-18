31f0919331c22fb1b3a50827a5cff0968f84ab26
martinpfh <martin@pfh.de>
2022-10-18 16:03:46 +0000

    Add cash and quick ratio

--- resources/ts/models/valuation.models.ts
+++ resources/ts/models/valuation.models.ts
@@ -5,16 +5,18 @@
 // Closed Source
 //
 
-import { SecurityProperties } from "@/types/security";
+import { RatioProperties } from "@/types/ratio";
 
-const valuations: Record<string, SecurityProperties>[] = [
+const valuations: Record<string, RatioProperties>[] = [
     { 'p/e': 'priceEarningsRatio' },
     { 'peg': 'priceEarningsToGrowthRatio' },
     { 'p/s': 'priceSalesRatio' },
     { 'p/b': 'priceToBookRatio' },
     { 'p/ocf': 'priceToOperatingCashFlowsRatio' },
     { 'p/fcf': 'priceToFreeCashFlowsRatio' },
-    { 'cash ratio': 'cashRatio' }
+    { 'cash ratio': 'cashRatio' },
+    { 'current ratio': 'currentRatio' },
+    { 'quick ratio': 'quickRatio' },
 ]
 
 export default valuations
