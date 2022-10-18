300649ca5f385c3124c07fc7130f06e66a9b8649
martinpfh <martin@pfh.de>
2022-10-18 16:04:43 +0000

    Put styls for card grid in an extra file

---
+++ resources/ts/Shared/Cards/Cards.styles.ts
@@ -0,0 +1,22 @@
+//
+// Licence
+// Copyright (c) 2009-2022 Stefan Martin
+// https://github.com/foxkill/planetcapital
+// Closed Source
+//
+
+export default {
+    cards: [
+        'grid-rows-auto',
+        'grid-cols-1',
+        'lg:grid-cols-child-size-lg',
+        'md:grid-cols-child-size-md', 
+        'sm:grid-cols-child-size-sm', 
+        'justify-center', 
+        'place-items-center', 
+        'gap-8', 
+        'bg-base-200', 
+        'pl-14', 
+        'pr-14',
+    ].join(" ")
+}
--- resources/ts/Shared/Cards/Cards.tsx
+++ resources/ts/Shared/Cards/Cards.tsx
@@ -9,11 +9,12 @@ import Card from "../Card";
 import IRatio from "@/types/ratio";
 import valuations from "../../models/valuation.models";
 import { useSecurity } from "../SecurityContext/SecurityContext";
-import Spinner from "../Spinner";
-import SelectPeriod from "../SelectPeriod/SelectPeriod";
+import styles from './Cards.styles'
 
 function HugeHeader({ children }: { children: ReactNode }): JSX.Element {
-    return <h2 className="min-h-4 !mt-0 !mb-16 uppercase text-center text-4xl font-bold">{children}</h2>
+    return <div className="max-w-md pb-6">
+        <h1 className="uppercase text-4xl font-bold">{children}</h1>
+    </div>
 }
 
 function Error({ error, children }: { error: any, children: ReactNode }): JSX.Element | null {
@@ -48,7 +49,7 @@ function Error({ error, children }: { error: any, children: ReactNode }): JSX.El
         // console.log('Error', error.message);
     }
     return (
-        <div className="alert alert-error shadow-lg mb-6">
+        <div className="alert alert-error shadow-lg mb-6 flex">
             <div>
                 <svg xmlns="http://www.w3.org/2000/svg" className="stroke-current flex-shrink-0 h-6 w-6" fill="none" viewBox="0 0 24 24"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>
                 <span>{msg}</span>
@@ -57,7 +58,11 @@ function Error({ error, children }: { error: any, children: ReactNode }): JSX.El
     )
 }
 
-export function Cards(): JSX.Element | null {
+interface ICardsProperties {
+    children: React.ReactNode
+}
+
+export function Cards(props: ICardsProperties): JSX.Element | null {
     const ctx = useSecurity()
 
     if (!ctx.context) {
@@ -65,40 +70,21 @@ export function Cards(): JSX.Element | null {
     }
 
     const { data, error, loading } = ctx.context.information
-    const visible = !!data
 
     return (
         <>
-            {/* Container */}
-            <div className={`${visible ? '' : 'hidden '}` + "container bg-base-200 mx-auto pt-4 pb-20 place-self-center"}>
-                {/* Huge Header */}
-                <HugeHeader>Valuation Multiples</HugeHeader>
-                <SelectPeriod></SelectPeriod>
-                {/* Grid */}
-                <div className="grid grid-rows-auto grid-cols-1 lg:grid-cols-child-size-lg  md:grid-cols-child-size-md sm:grid-cols-child-size-sm justify-center place-items-center gap-8 bg-base-200 pl-14 pr-14">
-                    {/* Loading Spinner */}
-                    {
-                        loading &&
-                        (
-                            <div className="hero bg-base-20 min-h-[60vh]">
-                                <div className="hero-content flex flex-col">
-                                    <div className="w-20">
-                                        <Spinner></Spinner>
-                                    </div>
-                                </div>
-                            </div>
-                        )
-                    }
-                    {
-                        (!loading && !error) &&
-                        valuations.map((value) => {
-                            const [key, val] = Object.entries(value)[0]
-                            return <Card type={ctx.context.periodType} key={key} ikey={val} data={data as IRatio}>{key}</Card>
-                        })
-                    }
-                    <Error error={error}>Es konnten keine Daten für diese Aktie geladen werden</Error>
-                </div>
+            <HugeHeader>{props.children}</HugeHeader>
+            <Error error={error}>Es konnten keine Daten für diese Aktie geladen werden</Error>
+            <div className={"grid " + styles.cards}>
+                {
+                    (!loading && !error) &&
+                    valuations.map((value) => {
+                        const [key, val] = Object.entries(value)[0]
+                        return <Card type={ctx.context.periodType} key={key} ikey={val} data={data as IRatio}>{key}</Card>
+                    })
+                }
             </div>
+            <p></p>
         </>
     )
 }
