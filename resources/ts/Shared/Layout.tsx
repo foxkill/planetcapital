f9c2faa4903f2bcd62d16c424ac66e4eb3d43154
martinpfh <martin@pfh.de>
2022-10-22 12:02:52 +0000

    Add a central layout

--- resources/ts/Shared/Layout.tsx
+++ resources/ts/Shared/Layout.tsx
@@ -1,33 +1,22 @@
-import React from 'react';
-import Hero from './Hero';
-import Footer from './Footer';
-import Navbar from './Navbar';
-import Cards from './Cards';
-import { SecurityContextProvider } from './SecurityContext/SecurityContext';
-import qc from "../planetapi/queryclient"
-import { QueryClientProvider } from 'react-query';
-import { ReactQueryDevtools } from "react-query/devtools";
-import CompanyInfo from './CompanyInfo';
-import TickerSearch from './TickerSearch';
-import SelectPeriod from './SelectPeriod/SelectPeriod';
+//
+// Licence
+// Copyright (c) 2009-2022 Stefan Martin
+// https://github.com/foxkill/planetcapital
+// Closed Source
+//
 
-const Layout = (): JSX.Element => {
+import React from "react";
+import Footer from "./Footer";
+import Navbar from "./Navbar";
+
+interface ILayoutProperities {
+    children: React.ReactNode
+}
+const Layout = ({ children }: ILayoutProperities): JSX.Element => {
     return (
         <>
             <Navbar />
-            <QueryClientProvider client={qc}>
-                <SecurityContextProvider>
-                    <Hero useColumnLayout={true} height={60}><TickerSearch></TickerSearch></Hero>
-                    <Hero height={30}><CompanyInfo /></Hero>
-                    <Hero useColumnLayout={false} height={60}>
-                        <Cards>
-                            Valuation Multiples
-                            <SelectPeriod></SelectPeriod>
-                        </Cards>
-                    </Hero>
-                </SecurityContextProvider>
-                <ReactQueryDevtools />
-            </QueryClientProvider>
+            {children}
             <Footer />
         </>
     )
