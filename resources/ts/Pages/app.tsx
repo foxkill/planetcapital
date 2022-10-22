cc526725b500e6fb07af96c5d75b0c189d9c035f
martinpfh <martin@pfh.de>
2022-10-22 11:59:08 +0000

    Show the main page

--- resources/ts/Pages/app.tsx
+++ resources/ts/Pages/app.tsx
@@ -6,9 +6,34 @@
 //
 
 import React from "react"
+import Layout from "@/Shared/Layout";
+import Hero from "@/Shared/Hero";
+import Cards from "@/Shared/Cards";
+import { SecurityContextProvider } from "@/Shared/SecurityContext/SecurityContext";
+import qc from "@/planetapi/queryclient"
+import { QueryClientProvider } from "react-query";
+import { ReactQueryDevtools } from "react-query/devtools";
+import CompanyInfo from "@/Shared/CompanyInfo";
+import TickerSearch from "@/Shared/TickerSearch";
+import SelectPeriod from "@/Shared/SelectPeriod/SelectPeriod";
 
 function App(): JSX.Element {
-    return (<h1>Ich bin eine Application</h1>)
+    return (
+        <QueryClientProvider client={qc}>
+            <SecurityContextProvider>
+                <Hero useColumnLayout={true} height={60}><TickerSearch></TickerSearch></Hero>
+                <Hero height={30}><CompanyInfo /></Hero>
+                <Hero useColumnLayout={false} height={60}>
+                    <Cards>
+                        Valuation Multiples
+                        <SelectPeriod></SelectPeriod>
+                    </Cards>
+                </Hero>
+            </SecurityContextProvider>
+            <ReactQueryDevtools />
+        </QueryClientProvider>
+    )
 }
 
+App.layout = (page: React.ReactNode): JSX.Element => <Layout>{page}</Layout>
 export default App
