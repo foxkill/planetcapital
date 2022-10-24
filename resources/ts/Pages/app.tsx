c07a7353241bb003d897620a2870957555975bf8
martinpfh <martin@pfh.de>
2022-10-24 08:04:42 +0000

    Remove react query context from Index page

--- resources/ts/Pages/app.tsx
+++ resources/ts/Pages/app.tsx
@@ -9,29 +9,23 @@ import React from "react"
 import Layout from "@/Shared/Layout";
 import Hero from "@/Shared/Hero";
 import Cards from "@/Shared/Cards";
-import { SecurityContextProvider } from "@/Shared/SecurityContext/SecurityContext";
-import qc from "@/planetapi/queryclient"
-import { QueryClientProvider } from "react-query";
-import { ReactQueryDevtools } from "react-query/devtools";
 import CompanyInfo from "@/Shared/CompanyInfo";
 import TickerSearch from "@/Shared/TickerSearch";
 import SelectPeriod from "@/Shared/SelectPeriod/SelectPeriod";
+import valuations from "@/models/valuation.models";
 
 function App(): JSX.Element {
     return (
-        <QueryClientProvider client={qc}>
-            <SecurityContextProvider>
-                <Hero useColumnLayout={true} height={60}><TickerSearch></TickerSearch></Hero>
-                <Hero height={30}><CompanyInfo /></Hero>
-                <Hero useColumnLayout={false} height={60}>
-                    <Cards>
-                        Valuation Multiples
-                        <SelectPeriod></SelectPeriod>
-                    </Cards>
-                </Hero>
-            </SecurityContextProvider>
-            <ReactQueryDevtools />
-        </QueryClientProvider>
+        <>
+            <Hero useColumnLayout={true} height={60}><TickerSearch></TickerSearch></Hero>
+            <Hero height={30}><CompanyInfo /></Hero>
+            <Hero useColumnLayout={false} height={60}>
+                <Cards valuations={valuations}>
+                    Valuation Multiples
+                    <SelectPeriod></SelectPeriod>
+                </Cards>
+            </Hero>
+        </>
     )
 }
 
