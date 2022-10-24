a682b41456cc50184b2f3d5b04be487330f90419
martinpfh <martin@pfh.de>
2022-10-24 08:05:08 +0000

    Continue relative valuation page

--- resources/ts/Pages/RelativeValuation/Index.tsx
+++ resources/ts/Pages/RelativeValuation/Index.tsx
@@ -9,12 +9,10 @@ import React from "react"
 import Layout from "@/Shared/Layout"
 import Hero from "@/Shared/Hero";
 import Cards from "@/Shared/Cards";
-import { SecurityContextProvider, useSecurity } from "@/Shared/SecurityContext/SecurityContext";
-import qc from "@/planetapi/queryclient"
-import { QueryClientProvider } from "react-query";
-import { ReactQueryDevtools } from "react-query/devtools";
 import CompanyInfo from "@/Shared/CompanyInfo";
 import SelectPeriod from "@/Shared/SelectPeriod/SelectPeriod";
+import valuations from "@/models/valuation.models";
+import { useSecurity } from "@/Shared/SecurityContext/SecurityContext";
 
 interface IRelativeValuationProperties {
     exchange: string,
@@ -25,28 +23,23 @@ interface IRelativeValuationProperties {
 
 function RelativeValuation({ exchange, symbol }: IRelativeValuationProperties): JSX.Element {
     const ctx = useSecurity()
-
-    console.log(ctx);
-    console.log(exchange, symbol)
-
-    if (ctx.setContext) {
-        ctx.setContext({...ctx.context, symbol, exchange })
-    }
-
-    return <Layout>
-        <QueryClientProvider client={qc}>
-            <SecurityContextProvider>
-                <Hero height={30}><CompanyInfo /></Hero>
-                <Hero useColumnLayout={false} height={60}>
-                    <Cards>
-                        2. Valuation Multiples
-                        {/* <SelectPeriod></SelectPeriod> */}
-                    </Cards>
-                </Hero>
-            </SecurityContextProvider>
-            <ReactQueryDevtools />
-        </QueryClientProvider>
-    </Layout>
+    if (ctx) {
+        if (!ctx.context.symbol) {
+            ctx.setContext({...ctx.context, symbol, exchange})
+        }
+    } 
+    return (
+        <>
+            <Hero height={30}><CompanyInfo /></Hero>
+            <Hero useColumnLayout={false} height={60}>
+                <Cards valuations={valuations}>
+                    2. Valuation Multiples
+                    <SelectPeriod></SelectPeriod>
+                </Cards>
+            </Hero>
+        </>
+    )
 }
 
+RelativeValuation.layout = (page: React.ReactNode): JSX.Element => <Layout>{page}</Layout>
 export default RelativeValuation
