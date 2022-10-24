55866621199ffa04bcd228cf673b84ce3cdc81f8
martinpfh <martin@pfh.de>
2022-10-24 08:01:46 +0000

    Add query provider to app

--- resources/ts/app.tsx
+++ resources/ts/app.tsx
@@ -3,11 +3,21 @@ import ReactDom from "react-dom/client"
 import React from "react";
 import { createInertiaApp } from "@inertiajs/inertia-react"
 import { InertiaProgress } from "@inertiajs/progress"
+import qc from "@/planetapi/queryclient"
+import { SecurityContextProvider } from "@/Shared/SecurityContext/SecurityContext";
+import { QueryClientProvider } from "react-query";
+// import { ReactQueryDevtools } from "react-query/devtools";
 
 createInertiaApp({
     resolve: (name) => import(/* @vite-ignore */ `./Pages/${name}`),
     setup({ el, App, props }) {
-        ReactDom.createRoot(el).render(<App {...props} />)
+        ReactDom.createRoot(el).render(
+            <QueryClientProvider client={qc}>
+                <SecurityContextProvider>
+                    <App {...props} />
+                </SecurityContextProvider>
+            </QueryClientProvider>
+        )
     }
 })
 
