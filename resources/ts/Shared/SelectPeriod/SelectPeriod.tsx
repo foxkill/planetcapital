5fb70445741e265b2e7459c454d89e6ea12e3f68
martinpfh <martin@pfh.de>
2022-11-05 18:13:52 +0000

    Make the naming of available periods more explicit

--- resources/ts/Shared/SelectPeriod/SelectPeriod.tsx
+++ resources/ts/Shared/SelectPeriod/SelectPeriod.tsx
@@ -11,14 +11,19 @@ import { useSecurity } from "../SecurityContext/SecurityContext"
 const SelectPeriod = (): JSX.Element => {
     const securityContext = useSecurity()
 
-    const periodTypes: PeriodTypes[] = ["FY", "TTM", "QTR"]
+    const periodTypes: Record<string, string>[] = [
+        { "TTM": "TTM" },
+        { "QTR": "Quarterly"},
+        { "FY": "Fiscal Year"},
+    ] 
 
+    const validPeriodTypes = periodTypes.map((v) => Object.keys(v)[0])
     const currentPeriodType = securityContext.context.periodType
 
     function setPeriod(event: React.MouseEvent<HTMLInputElement>): void {
         const periodType = (event.target as HTMLInputElement).getAttribute("data-value") as PeriodTypes
 
-        if (!periodType || periodTypes.indexOf(periodType) < 0) {
+        if (!periodType || validPeriodTypes.indexOf(periodType) < 0) {
             return
         }
 
@@ -30,12 +35,13 @@ const SelectPeriod = (): JSX.Element => {
     return (<div className="hero bg-base-200 min-h-[10vh]">
         <div className="hero-content gap-10">
             {
-                periodTypes.map((value, index) => {
+                periodTypes.map((obj, index) => {
+                    const [key, value] = Object.entries(obj).pop()!
                     return (
-                        <div className="form-control" key={value}>
+                        <div className="form-control" key={key}>
                             <label className="label cursor-pointer">
                                 <span className="label-text mr-5">{value}</span>
-                                <input key={index} onClick={(e: React.MouseEvent<HTMLInputElement>): void => setPeriod(e)} type="radio" data-value={value} name="radio-period" className="radio" defaultChecked={value == currentPeriodType} />
+                                <input key={index} onClick={(e: React.MouseEvent<HTMLInputElement>): void => setPeriod(e)} type="radio" data-value={key} name="radio-period" className="radio" defaultChecked={key == currentPeriodType} />
                             </label>
                         </div>
                     )
