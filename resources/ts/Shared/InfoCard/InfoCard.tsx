3aa7bbc5819ddbba16c267990d9acffa942f841a
martinpfh <martin@pfh.de>
2022-11-22 11:11:47 +0000

    Make infocard more flexible by passing icon

--- resources/ts/Shared/InfoCard/InfoCard.tsx
+++ resources/ts/Shared/InfoCard/InfoCard.tsx
@@ -20,13 +20,12 @@ function InfoCard({children, colSpan, caption, subheader, icon, image }: InfoCar
     return (
         <div className={`card card-bordered bg-base-100 pl-1 pr-4 ${colSpan}`}>
             <figure className="p-1">
+                {icon &&
                 <div className="align-middle w-12 text-center pl-2 pr-2">
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512" fill="#323B47" width={30} height={30}>
-                        {/* Font Awesome Pro 6.2.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc */}
-                        <path d="M151.6 469.6C145.5 476.2 137 480 128 480s-17.5-3.8-23.6-10.4l-88-96c-11.9-13-11.1-33.3 2-45.2s33.3-11.1 45.2 2L96 365.7V64c0-17.7 14.3-32 32-32s32 14.3 32 32V365.7l32.4-35.4c11.9-13 32.2-13.9 45.2-2s13.9 32.2 2 45.2l-88 96zM320 32h32c17.7 0 32 14.3 32 32s-14.3 32-32 32H320c-17.7 0-32-14.3-32-32s14.3-32 32-32zm0 128h96c17.7 0 32 14.3 32 32s-14.3 32-32 32H320c-17.7 0-32-14.3-32-32s14.3-32 32-32zm0 128H480c17.7 0 32 14.3 32 32s-14.3 32-32 32H320c-17.7 0-32-14.3-32-32s14.3-32 32-32zm0 128H544c17.7 0 32 14.3 32 32s-14.3 32-32 32H320c-17.7 0-32-14.3-32-32s14.3-32 32-32z"/>
-                    </svg>
+                    {icon}
                 </div>
-                <h2 className="text-lg font-bold w-full">
+                }
+                <h2 className="pl-4 text-lg font-bold w-full">
                     {caption}
                     <div className="text-sm font-thin">{subheader}</div>
                 </h2>
