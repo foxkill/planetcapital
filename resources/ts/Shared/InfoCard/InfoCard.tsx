8a1524c76de7e79a2570cc91d21633eb6df2aff4
martinpfh <martin@pfh.de>
2022-12-11 18:50:32 +0000

    Make colspan property optional

--- resources/ts/Shared/InfoCard/InfoCard.tsx
+++ resources/ts/Shared/InfoCard/InfoCard.tsx
@@ -9,34 +9,35 @@ import React from "react"
 
 interface InfoCardProperties {
     children: React.ReactNode
-    colSpan: string
+    colSpan?: string
     header: string
     subheader: string
     icon?: React.ReactNode
     image?: string | undefined
 }
 
-function InfoCard({children, colSpan, header: caption, subheader, icon, image }: InfoCardProperties): JSX.Element {
+function InfoCard({ children, colSpan, header: caption, subheader, icon, image }: InfoCardProperties): JSX.Element {
+    const cs = colSpan ?? ""
     return (
-        <div className={`card card-bordered bg-base-100 pl-1 pt-1 pr-2 ${colSpan}`}>
-            <figure className="p-1">
+        <div className={`card card-bordered bg-base-100 pt-2 pr-2 ${cs}`}>
+            <figure className="">
                 {icon &&
-                <div className="align-middle w-12 text-center pl-2 pr-2">
-                    {icon}
-                </div>
+                    <div className="align-middle w-12 text-center pl-2 pr-2">
+                        {icon}
+                    </div>
                 }
                 <h2 className="pl-4 text-lg font-bold w-full">
                     {caption}
                     <div className="text-sm font-thin">{subheader}</div>
                 </h2>
-                {image && 
+                {image &&
                     <div>
-                        <img className="rounded" id="company-logo" src={image} width="60" height="60"></img>
+                        <img className="" id="company-logo" src={image} width="32" height="32"></img>
                     </div>
                 }
             </figure>
             <hr className="mt-1"></hr>
-            <div className="card-body items-center flex-row">
+            <div className="card-body items-center">
                 {children}
             </div>
         </div>
