cfe2977966bf3404ddc916df743dd45d50aa5a61
martinpfh <martin@pfh.de>
2022-12-11 18:52:28 +0000

    Put lastvalue accessor in try/catch

--- resources/ts/Shared/Charts/ExtendedHistoryChart.tsx
+++ resources/ts/Shared/Charts/ExtendedHistoryChart.tsx
@@ -5,17 +5,17 @@
 // Closed Source
 //
 
-import React, { Fragment, useMemo } from "react"
+import React, { Fragment } from "react"
 import { ResponsiveBar, BarDatum } from "@nivo/bar"
 import { IKeyMetric } from "@/types/key-metric"
-import { Bar } from "@nivo/bar";
 import { timeFormat, timeParse } from "d3-time-format"
 import { PaletteColors } from "react-palette";
-import moneyformat from "@/utils/moneyformat";
+import IRatio from "@/types/ratio";
 
 interface ExtendedHistoryChartProps {
-    children: React.ReactNode
-    metrics: IKeyMetric[]
+    children?: React.ReactNode
+    metric: string
+    metrics: IKeyMetric[] | IRatio[]
     metricKey: string
     periodType: string
     palette: PaletteColors
@@ -33,6 +33,7 @@ const BAR_MAX_WIDTH = 12
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 const CustomBar = ({ bar }: { bar: Bar; }): JSX.Element => {
+    return <></>
     // const w = width > BAR_MAX_WIDTH ? BAR_MAX_WIDTH : width;
     // return (
     //     <rect
@@ -80,91 +81,94 @@ const Line = ({ bars, xScale, yScale }): JSX.Element => {
 };
 
 const ExtendedHistoryChart: React.FC<ExtendedHistoryChartProps> = (props) => {
-    if (!props.metrics || props.metrics.length == 0) {
+    if (!props.metrics || props.metrics.length === 0) {
         return null
     }
 
-    const lastValue = props.metrics[0][props.metricKey] ?? 0
-    
+    let lastValue = 0
+    try {
+        lastValue = props.metrics[0][props.metricKey] ?? 0
+    } catch (error) {
+        console.log(error);
+    }
+
     const parseTime = timeParse("%Y-%m-%d")
     const formatTimeYear = timeFormat("%Y")
     const formatTimeDefault = timeFormat("%b-%Y")
 
     return (
         <>
-            <div className="text-center w-96">
-                <div className="text-4xl pb-8">{(lastValue * 100).toFixed(1)}%</div>
-                <div className="h-32">
-                    <ResponsiveBar
-                        data={props.metrics as unknown as BarDatum[]}
-                        indexBy="date"
-                        keys={[props.metricKey]}
-                        margin={{ top: 20, right: 0, bottom: 20, left: 0 }}
-                        valueScale={{ type: "linear" }}
-                        enableLabel={false}
-                        enableGridY={false}
-                        axisBottom={null}
-                        axisTop={null}
-                        axisLeft={null}
-                        axisRight={null}
-                        animate
-                        padding={0.8}
-                        colors={[props.palette.vibrant!]}
-                        borderRadius={2}
-                        // layers={["grid", "axes", "bars", "markers", "legends"]}
-                        // innerPadding={4}
-                        // barComponent={c}
-                        tooltip={(dataPoint): JSX.Element => {
-                            const timerange = parseTime((dataPoint.data as unknown as IKeyMetric).date)
+            <div className="text-4xl pb-6">{(lastValue * 100).toFixed(1)}%</div>
+            <div className="h-32 w-96 min-w-full">
+                <ResponsiveBar
+                    data={props.metrics as unknown as BarDatum[]}
+                    indexBy="date"
+                    keys={[props.metricKey]}
+                    margin={{ top: 10, right: 0, bottom: 5, left: 0 }}
+                    valueScale={{ type: "linear" }}
+                    enableLabel={false}
+                    enableGridY={false}
+                    axisBottom={null}
+                    axisTop={null}
+                    axisLeft={null}
+                    axisRight={null}
+                    animate
+                    padding={0.8}
+                    colors={[props.palette.vibrant!]}
+                    borderRadius={2}
+                    // layers={["grid", "axes", "bars", "markers", "legends"]}
+                    // innerPadding={4}
+                    // barComponent={c}
+                    tooltip={(dataPoint): JSX.Element => {
+                        const timerange = parseTime((dataPoint.data as unknown as IKeyMetric).date)
 
-                            if (null === timerange) {
-                                return <></>
-                            }
+                        if (null === timerange) {
+                            return <></>
+                        }
 
-                            let tmr = ""
-                            const period = (dataPoint.data as  any as IKeyMetric).period
+                        let tmr = ""
+                        const period = (dataPoint.data as any as IKeyMetric).period
 
-                            switch (period) {
-                                case "FY":
-                                case "TTM": 
-                                    tmr = period + " " + formatTimeYear(timerange)
+                        switch (period) {
+                            case "FY":
+                            case "TTM":
+                                tmr = period + " " + formatTimeYear(timerange)
                                 break
-                                default:
-                                    tmr = formatTimeDefault(timerange) + " " + period
-                            }
-                            // const period = formatTimeDefault(timerange) + " " + (dataPoint.data as  any as IKeyMetric).period
+                            default:
+                                tmr = formatTimeDefault(timerange) + " " + period
+                        }
+                        // const period = formatTimeDefault(timerange) + " " + (dataPoint.data as  any as IKeyMetric).period
 
-                            // const tmr = formatTimeQT(timerange) + " " + dataPoint.data.period
-                            const background = props.palette.darkMuted
-                            const color = props.palette.lightVibrant
-                            // const delimiterColor = props.palette.lightMuted
-                            
-                            return (
-                                <>
-                                    <div className="alert shadow-lg">
-                                        <div className="flex items-center gap-2">
-                                            <div>
-                                                <div className="min-w-full pb-1 text-center">
-                                                    <div className="align-middle inline-block w-4 h-4" style={{ backgroundColor: color, border: "1px solid " + background }}>
-                                                    </div>
-                                                    <div className="inline-block leading-4 align-middle indent-2">{tmr}</div>
+                        // const tmr = formatTimeQT(timerange) + " " + dataPoint.data.period
+                        const background = props.palette.darkMuted
+                        const color = props.palette.lightVibrant
+                        // const delimiterColor = props.palette.lightMuted
+
+                        return (
+                            <>
+                                <div className="alert shadow-lg bg-opacity-10 backdrop-blur">
+                                    <div className="flex items-center gap-2">
+                                        <div className="text-center">
+                                            <div className="min-w-full pb-1">
+                                                <div className="align-middle inline-block w-4 h-4" style={{ backgroundColor: color, border: "1px solid " + background }}>
                                                 </div>
-                                                <div className="text-xs pb-2 text-base-500">Historical Value</div>
-                                                {/* <div>Historical Value</div> */}
-                                                <hr className="border-1 border-slate-300 pb-1"></hr>
-                                                <div className="font-bold">
-                                                    {/* TODO: prevent -0% values */}
-                                                    {(dataPoint.value * 100).toFixed(1)}%
-                                                </div>
-                                                <span className="uppercase">{dataPoint.id}</span>
+                                                <div className="inline-block leading-4 align-middle indent-2">{tmr}</div>
                                             </div>
+                                            <div className="text-xs pb-2 text-base-500">Historical Value</div>
+                                            {/* <div>Historical Value</div> */}
+                                            <hr className="border-1 border-slate-300 pb-1"></hr>
+                                            <div className="font-bold">
+                                                {/* TODO: prevent -0% values */}
+                                                {(dataPoint.value * 100).toFixed(1)}%
+                                            </div>
+                                            <span className="uppercase">{props.metric}</span>
                                         </div>
                                     </div>
-                                </>
-                            )
-                        }}
-                    />
-                </div>
+                                </div>
+                            </>
+                        )
+                    }}
+                />
             </div>
         </>
     )
