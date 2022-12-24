1322c1576d351ab0eefcfa0625b46175aa65b031
martinpfh <martin@pfh.de>
2022-12-24 12:23:25 +0000

    Add function for handling zero values.

--- resources/ts/Shared/StatementCard/StatementCard.tsx
+++ resources/ts/Shared/StatementCard/StatementCard.tsx
@@ -8,9 +8,7 @@ import React from "react"
 import moneyformat from "@/utils/moneyformat"
 import IIncomeStatement from "@/types/income-statement"
 import ValueIndicator from "../ValueIndicator/ValueIndicator"
-import yoy from "@/utils/yoy"
-import cagr from "@/utils/cagr"
-import calculateAverageGrowth from "@/utils/growth"
+import { calculateCagr } from "@/utils/preparecalc"
 
 interface IStatementCardProperties {
     children: React.ReactNode
@@ -21,76 +19,9 @@ interface IStatementCardProperties {
     dataKey: string
 }
 
-function calculatePerformance(data: IIncomeStatement[], dataKey: string, length: number, periodType: string): number {
-    let index = length
 
-    if (periodType != "FY") {
-        index = (length * 4) - 1
-    }
-
-    const currentValue = data[0][dataKey]
-    const priorValue = data[index][dataKey]
-
-    return yoy(currentValue, priorValue)
-}
-
-function calculateCagr(data: IIncomeStatement[], dataKey: string, periods: number, periodType: string): number {
-    let index = periods
-
-    if (periodType != "FY") {
-        index = (periods << 2)
-    }
-
-    const currentValue = data[0][dataKey]
-    const priorValue = data[index][dataKey]
-
-    const log = (dataKey == "netIncome" && periods==5)
-
-    if (log) {
-        console.log(index, periods, currentValue, priorValue);
-    }
-    
-    const r = cagr(priorValue, currentValue, periods)
-
-    return r.percentage
-}
-
-
-function calculateGrowth(startValue, endValue): number {
-    return ((endValue - startValue) / startValue) * 100;
-}
-
-function calculateAvgGrowth(data: IIncomeStatement[], dataKey: string, length: number, periodType: string): number {
-    let totalGrowth = 0;
-    let quarters = (periodType === "FY") ? length : (length << 2)
-
-    const log = (dataKey == "netIncome" && length==5)
-    let lastgrowth: number[] = []
-    let k = 0
-    if (log) {
-        // k = 1
-        // quarters += k
-    }
-    for (let i = k; i < quarters; i++) {
-        const currentSale = data[i][dataKey]
-        let previousSale = data[i + 1][dataKey];
-        const growth = calculateGrowth(previousSale, currentSale)
-        if (log) {
-            lastgrowth.push(growth)
-            // console.log(growth.toFixed(0), currentSale, previousSale)
-        }
-        totalGrowth += growth
-    }
-    if (log) {
-        console.log(data.length, quarters, totalGrowth, lastgrowth);
-        
-        console.log(data.map((v) => v[dataKey]))
-        // console.log(totalGrowth, length, quarters, lastgrowth);
-        //    console.log(moneyformat(lastgrowth, false, 1))
-        // console.log(data.map(v => (v.netIncome)));
-
-    }
-    return (totalGrowth / length)
+function handleZeroAmount(value: number): number {
+    return (Math.round(value) == 0) ? 0 : value
 }
 
 //
@@ -100,23 +31,26 @@ const StatementCard: React.FC<IStatementCardProperties> = (props): JSX.Element |
     const { data, caption, children, isLoading, dataKey, periodType } = props
 
     const fractionDigits = 0
-    // let data = props.data
 
-    let fiveYearPerf = ""
-    let threeYearPerf = ""
-    let oneYearPerf = ""
+    let fiveYearPerf = 0
+    let threeYearPerf = 0
+    let oneYearPerf = 0
 
     if (data && data.length >= 6) {
-        fiveYearPerf = calculateCagr(data, dataKey, 5, periodType).toFixed(fractionDigits)
+        // TODO: remove.
+        if (dataKey == "netIncome") {
+            console.log(data.map((v) => v[dataKey]));
+        }
+
+        fiveYearPerf = handleZeroAmount(calculateCagr(data, dataKey, 5, periodType))
     }
 
     if (data && data.length >= 4) {
-        threeYearPerf = calculateCagr(data, dataKey, 3, periodType).toFixed(fractionDigits)
+        threeYearPerf = handleZeroAmount(calculateCagr(data, dataKey, 3, periodType))
     }
 
     if (data && data.length >= 2) {
-        // Calculate AAGR Performance.
-        oneYearPerf = calculateCagr(data, dataKey, 1, periodType).toFixed(fractionDigits)
+        oneYearPerf = handleZeroAmount(calculateCagr(data, dataKey, 1, periodType))
     }
 
     let value = 0.0
@@ -128,6 +62,9 @@ const StatementCard: React.FC<IStatementCardProperties> = (props): JSX.Element |
         // Code.       
     }
 
+
+    const performance = [{1: oneYearPerf}, {3: threeYearPerf}, {5:fiveYearPerf}]
+
     return (
         <div className="card bg-base-100 hover:shadow-xl p-8">
             <div className="text-center text-5xl">{moneyformat(value)}</div>
@@ -135,36 +72,23 @@ const StatementCard: React.FC<IStatementCardProperties> = (props): JSX.Element |
                 {caption}
             </div>
             <div className="card-body p-0">
-                {isLoading ? (<div className="text-center">{children}</div>) : (
+                {isLoading ? (<div className="overflow-x-auto text-center">{children}</div>) : (
                     <div className="overflow-x-auto">
                         <table className="table table-compact w-full text-slate-500">
                             <tbody>
-                                {/* 1 Year */}
-                                {oneYearPerf &&
-                                    <tr className="hover">
-                                        <th className="rounded-none">1 Year</th>
-                                        <td className="rounded-none text-right border-b-0">
-                                            <ValueIndicator unit="%">{oneYearPerf}</ValueIndicator>
-                                        </td>
-                                    </tr>
-                                }
-                                {/* 3 Years */}
-                                {threeYearPerf &&
-                                    <tr className="hover">
-                                        <th className="rounded-none">3 Years</th>
-                                        <td className="rounded-none text-right border-b-0">
-                                            <ValueIndicator unit="%">{threeYearPerf}</ValueIndicator>
-                                        </td>
-                                    </tr>
-                                }
-                                {/* 5 Years */}
-                                {fiveYearPerf &&
-                                    <tr className="hover">
-                                        <th className="rounded-none">5 Years</th>
-                                        <td className="rounded-none text-right border-b-0">
-                                            <ValueIndicator unit="%">{fiveYearPerf}</ValueIndicator>
-                                        </td>
-                                    </tr>
+                                {performance.map((perf) => {
+                                    if (Number.isNaN(perf)) return <></>
+                                    const [key, value] = Object.entries(perf)[0];
+                                    
+                                    return (
+                                        <tr key={key} className="hover">
+                                            <th className="rounded-none">{key} Year</th>
+                                            <td className="rounded-none text-right border-b-0">
+                                                <ValueIndicator unit="%">{value.toFixed(0)}</ValueIndicator>
+                                            </td>
+                                        </tr>
+                                    )
+                                })
                                 }
                             </tbody>
                         </table>
