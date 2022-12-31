a678e38a4cef14f77cb00ddf895c548ea0395b51
martinpfh <martin@pfh.de>
2022-12-31 12:16:59 +0000

    Add Spacer component

---
+++ resources/ts/Shared/Spacer/Spacer.tsx
@@ -0,0 +1,13 @@
+//
+// Licence
+// Copyright (c) 2009-2022 Stefan Martin
+// https://github.com/foxkill/planetcapital
+// Closed Source
+//
+import React from "react"
+
+const Spacer: React.FC = () => {
+    return <div className="h-8"></div>
+}
+
+export default Spacer;
