27636be59afa9aa472eba385e22350266a8a09f1
martinpfh <martin@pfh.de>
2023-01-06 10:47:00 +0000

    Start sections, beginning with CompanyInformation

---
+++ resources/ts/Shared/Sections/CompanyInformation/CompanyInformation.tsx
@@ -0,0 +1,19 @@
+//
+// Licence
+// Copyright (c) 2009-2023 Stefan Martin
+// https://github.com/foxkill/planetcapital
+// Closed Source
+//
+import React from "react"
+
+type CompanyInformationProps = {
+
+}
+
+const CompanyInformation: React.FC = () => {
+    return (
+        <div>Company Information</div>
+    )
+}
+
+export default CompanyInformation
---
+++ resources/ts/Shared/Sections/CompanyInformation/index.ts
@@ -0,0 +1,7 @@
+//
+// Licence
+// Copyright (c) 2009-2023 Stefan Martin
+// https://github.com/foxkill/planetcapital
+// Closed Source
+//
+export { default } from "./CompanyInformation"
