b90429f521a5354e23d3cf1d8fff4bbc66ca1f74
martinpfh <martin@pfh.de>
2023-01-07 17:25:25 +0000

    Add types for enterprise valuations

--- resources/ts/models/valuation.models.ts
+++ resources/ts/models/valuation.models.ts
@@ -5,20 +5,44 @@
 // Closed Source
 //
 
-import { RatioProperties } from "@/types/ratio";
+import ICashflowStatement from "@/types/cashflow-statement";
+import { IKeyMetric } from "@/types/key-metric";
+import IRatio, { RatioProperties } from "@/types/ratio";
+
+type K = IKeyMetric | IRatio | ICashflowStatement
+type RatioCalculationFunc = <T extends K>(data: T[], numerator: string, denominator: string) => number
 
 const valuations: Record<string, RatioProperties>[] = [
-    { "p/e": "priceEarningsRatio" },
-    { "peg": "priceEarningsToGrowthRatio" },
     { "p/s": "priceToSalesRatio" },
-    { "p/b": "priceToBookRatio" },
+    { "p/e": "priceEarningsRatio" },
     { "p/ocf": "priceToOperatingCashFlowsRatio" },
     { "p/fcf": "priceToFreeCashFlowsRatio" },
+    { "p/b": "priceToBookRatio" },
+    { "peg": "priceEarningsToGrowthRatio" },
     { "cash ratio": "cashRatio" },
     { "current ratio": "currentRatio" },
     { "quick ratio": "quickRatio" },
 ]
 
-export type Valuations = typeof valuations
+const ratio: RatioCalculationFunc = <T>(data: T, numerator: string, denominator: string): number => {
+    return data[numerator] / data[denominator]
+}
 
+// ev/gp - Enterprice Value / Gross Profit
+// ev/fcff - Enterprice Value / Free Cash Flow To Firm
+// ev/fcfe - Enterprice Value / Free Cash Flow To Equity
+// ev/ic - Enterprice Value / Investing Capital
+// ev - IKeyMetric
+const enterpriseValuations: Record<string, keyof IKeyMetric | RatioCalculationFunc | "-">[] = [
+    { "ev/s": "evToSales" },
+    { "ev/ocf": "evToOperatingCashFlow" },
+    { "ev/fcf": "evToFreeCashFlow" },
+    { "ev/ebit": "-" },
+    { "ev/ic": "-" },
+    { "ev/fcff": "-" },
+    { "ev/gp": ratio }
+]
+
+export type Valuations = typeof valuations
+export {enterpriseValuations}
 export default valuations
