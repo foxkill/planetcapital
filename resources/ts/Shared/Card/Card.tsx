f142cf70dcdb465e5130c8ae9d5ef7ebf19dcd28
martinpfh <martin@pfh.de>
2024-04-10 08:40:03 +0000

    Add correct github url to header

--- resources/ts/Shared/Card/Card.tsx
+++ resources/ts/Shared/Card/Card.tsx
@@ -1,7 +1,7 @@
 //
 // Licence
 // Copyright (c) 2009-2022 Stefan Martin
-// https://github.com/foxkill/\var\www\html
+// https://github.com/foxkill/planetcapital
 // Closed Source
 //
 import React from "react";
